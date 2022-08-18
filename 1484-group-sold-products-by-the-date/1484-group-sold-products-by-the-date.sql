2280590c37ac5f42b05870b0376b386817b73ade
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-08-18 20:46:49 +0530

    Update 1484-group-sold-products-by-the-date.sql

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+/*The GROUP BY statement groups rows that have the same values into summary rows.
+The GROUP BY statement is often used with aggregate functions (COUNT(), MAX(), MIN(), SUM(), AVG()) to group the result-set by one or more columns.
+Another group method we use is GROUP_CONCAT, which concat items of grouped rows and joins them by using "," seperator.
+*/
 select sell_date, 
     count(distinct(product)) as "num_sold", 
     group_concat(distinct product order by product asc) as "products" 
