c86b05f5420daeb324abe2ea6e4b04a02e9d2dad
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-09-12 17:36:04 +0530

    Time: 17 ms (29.47%), Space: 10.7 MB (15.81%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int thirdMax(vector<int>& nums) {
-        set<int,greater<int>> s;
-        
-        for(int i=0; i<nums.size() ; i++)
-            s.insert(nums[i]);
-        
-        if(s.size()<3)
-            return *s.begin();
-        
-        int ans=0;
-        int itr=1;
-        for(auto i : s){
-            if(itr==3){
-                ans = i;
-                return ans;
-            }
-            itr++;
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        if(tokens.size()==0){
+            return 0;
+        } 
+        if(tokens.size()==1){
+            return power > tokens[0] ? 1 : 0;
         }
-        return ans;
+        sort(tokens.begin(),tokens.end());
+        int lo = 0, hi = tokens.size()-1;
+        int score = 0;
+        while(lo<hi){
+            if(power>=tokens[lo]){
+                power -= tokens[lo];
+                score++; lo++;
+            }
+            else if(score >= 1){
+                power += tokens[hi];
+                score--; hi--;
+            } else {
+                lo++; hi--;
+            }
+        }
+        
+        if(lo==hi && power>=tokens[lo]){
+            score++;
+        }
+        return score;
     }
 };
