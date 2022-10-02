7d6f5741f282e86816a68406525c617dc2f62c5f
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-10-02 05:45:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-int dfs(int n, vector<int> &dp){
-    if(n==0)
-        return 1;
-    if(n<0)
-        return 0;
-    if(dp[n] != -1)
-        return dp[n];
-    return dp[n] = dfs(n-1,dp)+dfs(n-2,dp);
-}
     int climbStairs(int n) {
-    vector<int> dp(n+1,-1);
-    return dfs(n,dp);        
+        // Proper DP soln.
+        // if (n == 1) return 1;
+        // if (n == 2) return 2;
+        // std::vector<int> opt(n, 0);
+        // opt[0] = 1;
+        // opt[1] = 2;
+        // for (int i = 2; i < n; i++) {
+        //     opt[i] = opt[i - 1] + opt[i-2];
+        // }
+        // return opt[n - 1];
+        
+        // Optimized
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int oldest = 1;
+        int middle = 2;
+        int newest = 0;
+        
+        for (int i = 2; i < n; ++i) {
+            newest = oldest + middle;
+            oldest = middle;
+            middle = newest;
+        }
+        return newest;
     }
 };
