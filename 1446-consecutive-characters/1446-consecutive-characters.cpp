9f01545123efee89aef724cbebdad2cb22e894d3
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-10-03 23:30:40 +0530

    Time: 11 ms (18.71%), Space: 6.8 MB (71.73%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int maxPower(string s) {
-        if(s.length() < 1)
-            return 0;
-        int cnt=1;
-        int mx=1;
-        
-        for(int i=1 ; i<s.length() ; i++){
-            if(s[i] == s[i-1]){
-                cnt++;
-            } else {
-                cnt=1;
+        int mx=1,c=1;
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1]){
+                c++;
+            }else{
+                mx=max(mx,c);
+                c=1;
             }
-            mx = max(mx,cnt);
         }
+        mx=max(mx,c);
         return mx;
     }
 };
