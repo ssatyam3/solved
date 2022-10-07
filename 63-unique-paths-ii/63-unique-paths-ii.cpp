aaff8a93b97aecd04a053b98848a4e62554aa911
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-10-08 02:25:36 +0530

    Time: 4 ms (70.47%), Space: 8 MB (13.61%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -24,8 +24,25 @@ public:
             return 0;
         } 
         
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,0));
         
-        return f(m-1,n-1,grid,dp);
+        for(int i=0 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else {
+                        int l=0,r=0;
+                        if(i>0 && grid[i-1][j] == 0) {
+                            l = dp[i-1][j];
+                        }
+                        if(j>0 && grid[i][j-1]==0) 
+                            r = dp[i][j-1];
+                        dp[i][j] = l+r;
+                    } 
+                }
+            }
+        
+        
+        return dp[m-1][n-1];
     }
 };
