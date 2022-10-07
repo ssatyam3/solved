6e4b42118e063eb948be1f83e49872fe737b8159
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-10-08 02:33:35 +0530

    Time: 3 ms (87.05%), Space: 7.8 MB (45.76%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    // Space optimized solution
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
