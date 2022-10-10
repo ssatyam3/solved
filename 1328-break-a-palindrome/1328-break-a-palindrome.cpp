926525632da1bab62e6d83b26ff6e7046bec4727
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-10-10 17:22:58 +0530

    Time: 4 ms (29.56%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-    bool isPalin(string s){
-        if(s.length()<2){
-            return true;
-        }
-        int n = s.length();
-        for(int i=0; i<n/2 ; i++){
-            if(s[i] != s[n-i-1]){
-                return false;
+    string breakPalindrome(string S) {
+        int n = S.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (S[i] != 'a') {
+                S[i] = 'a';
+                return S;
             }
         }
-        return true;
-    }
-    string breakPalindrome(string s) {
-        int n = s.length();
-        if(n==1) return "";
-        
-        int nnn = 97;
-        for(int i=0 ; i<=n ; i++){
-            if(i==n){
-                s[n-1] = (char)(++nnn);
-                return s;
-            } else if(s[i] != (char)nnn){
-                int ch = s[i];
-                s[i] = (char)(nnn);
-                if(isPalin(s)){
-                    s[i] = ch;
-                } else {
-                    return s;
-                }
-            }    
-        }
-        
-        return s;
+        S[n - 1] = 'b';
+        return n < 2 ? "" : S;
     }
 };
