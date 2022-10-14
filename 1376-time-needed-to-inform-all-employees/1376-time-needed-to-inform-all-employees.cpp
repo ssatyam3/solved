97ea4bdfbbddc2f5585418f81d81cb67649441d9
ssatyam3 <94914072+ssatyam3@users.noreply.github.com>
2022-10-15 00:48:11 +0530

    Time: 1001 ms (14.75%), Space: 160 MB (16.02%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int dfs(int node, int prevTime, vector<int> &vis,unordered_map<int,vector<int>> &adj, vector<int>& informTime){
+        vis[node] = 1;
+        int levelTime = informTime[node];
+        int mx=0;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                int time = dfs(it, levelTime , vis,adj, informTime);
+                mx = max(time,mx);
+            }
+        }
+        return mx+prevTime;
+    }
+    
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        
+        unordered_map<int,vector<int>> adj;
+        vector<int> vis(n,0);
+        for(int v=0 ; v<manager.size() ; v++){
+            if(manager[v]==-1){
+                continue;
+            }
+            int u = manager[v];
+            adj[u].push_back(v);
+        }
+        int ans=dfs(headID,0, vis, adj, informTime);
+        return ans;
+    }
+};
